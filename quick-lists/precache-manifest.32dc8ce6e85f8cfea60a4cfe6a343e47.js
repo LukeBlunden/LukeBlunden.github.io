b864ba7c4fdf8f20df21264443098bb50c9ce538
luke mcdonald <lucas.blunden@gmail.com>
2020-05-03 10:31:14 +1000

    Quick lists commit

---
+++ quick-lists/precache-manifest.32dc8ce6e85f8cfea60a4cfe6a343e47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497bd52035ee58d516ccf9157b93ae8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "785384a4171cde0c4566",
+    "url": "/static/css/main.2bd7961e.chunk.css"
+  },
+  {
+    "revision": "70d79df32becca2fd70c",
+    "url": "/static/js/2.8f4e23b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8f4e23b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "785384a4171cde0c4566",
+    "url": "/static/js/main.83d546e8.chunk.js"
+  },
+  {
+    "revision": "54e183a7688c67a35393",
+    "url": "/static/js/runtime-main.523755e7.js"
+  }
+]);
